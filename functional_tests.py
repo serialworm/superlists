301a573d50ba69d0856545d21a572cd7262be26b
Eric Beringer <eric.beringer@thinksolid.com>
2014-08-28 19:56:49 -0500

    Created helper method for list items

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # As a user I can visit the home page
@@ -36,17 +41,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # When I press the enter key the data in the form is saved
-        # and the text I previously entered no appears as
+        # and the text I previously entered now appears as
         # a todo item on the page
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2. Use peacock feathers to make a fly',
-                      [row.text for row in rows])
-
         # As a user I still see a place to enter another todo
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use the feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('2. Use the feathers to make a fly')
+
         self.fail('Finish the test!')
 
 
